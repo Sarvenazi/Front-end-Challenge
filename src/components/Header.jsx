b576da4c9aa1e9545a12cd9d2392aec447f32084
sarvenaz <samadloui@gmail.com>
2022-11-11 15:17:27 +0330

    add header and responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Grid, Typography , Avatar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+const whiteColor = {
+  color: "white",
+};
 
 function Header() {
-  return <div>Header</div>;
+  const matches = useMediaQuery('(min-width:527px)');
+  return (
+    <Grid
+      sx={{ bgcolor: "#6154C2", height: "60px", px: 5 , position:"absolute",width:"100%"}}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Grid sx={whiteColor}>
+        <Typography variant="body1">
+          HARBOUR.SPACE{" "}
+          <span style={{ fontSize: "11px" }}>/INTERACTION DESIGN</span>
+        </Typography>
+      </Grid>
+      {matches ? <Avatar className="applybtn" 
+      sx={{position:"absolute" , 
+       width: 100, height: 100 , right:100 , top:10 ,
+        bgcolor:"#5ACF81" , textAlign:"center" }} >APPLY NOW</Avatar> : null}
+      <Grid sx={whiteColor}>
+        <MenuIcon color="white" />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Header;
